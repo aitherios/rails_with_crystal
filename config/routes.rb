100421c026a744c98342fc9f2f54b377600d677a
Renan Mendes Carvalho <aitherios@gmail.com>
2015-01-06 10:04:00 -0200

    Ver https://github.com/jruby/jruby/issues/376

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,8 @@ RailsWithCrystal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  resources :reports
   root :to => 'reports#index'
+  get 'report' => 'reports#index'
 
   # See how all your routes lay out with "rake routes"
 
