5787b9340c88cac034c243ac31d7d6f73fd53f8f
Renan Mendes Carvalho <aitherios@gmail.com>
2015-01-06 10:03:14 -0200

    Pequena alteração para ser compatível com jruby 1.6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RailsWithCrystal::Application.config.session_store :cookie_store, key: '_rails_with_crystal_session'
+RailsWithCrystal::Application.config.session_store(:cookie_store, :key => '_rails_with_crystal_session')
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
